b30409ef26d7ae31f2c5bcf2bc90e0c33963f542
ak <auxgts@gmail.com>
2021-04-16 13:27:27 +0530

    image-picker

--- src/screens/raise/raise-hazard.tsx
+++ src/screens/raise/raise-hazard.tsx
@@ -16,6 +16,7 @@ import {RouteProp} from '@react-navigation/native';
 import {RaiseHazardStackParamList} from '../../navigation/raise-hazard';
 import InputBox from './components/input';
 import DropDownPicker from '../../components/dropdown-picker';
+import * as ImagePicker from 'react-native-image-picker';
 import {Icon} from 'native-base';
 
 type RaiseNavigationProp = StackNavigationProp<
@@ -30,6 +31,7 @@ interface Props {
 
 type State = {
   refreshing: boolean;
+  AttachedPhotoList: Array<any>;
 };
 
 export default class RaiseHazard extends React.PureComponent<Props, State> {
@@ -39,7 +41,18 @@ export default class RaiseHazard extends React.PureComponent<Props, State> {
       refreshing: false,
     };
   }
-
+  handleChoosePhoto = () => {
+    ImagePicker.launchImageLibrary({
+      mediaType: "photo",
+      maxHeight: 300,
+      maxWidth: 300,
+    }, (response) => {
+      if (response.uri && response.fileSize! < 41052) {
+        this.setState({ photo: [...this.state.photo, response] });
+        AttachedPhotoList.push(response)
+      }
+    });
+  };
   render() {
     return (
       <View style={styles.container}>
@@ -52,119 +65,145 @@ export default class RaiseHazard extends React.PureComponent<Props, State> {
               refreshing={this.state.refreshing}
               title="Loading..."
             />
-          
           }>
-          <Text style={{fontSize: 24, fontWeight: '700'}}>Hazard Details</Text>
-          <Text style={{fontSize: 16}}>Hazard name</Text>
-          <InputBox
-            height={40}
-            marginHorizontal={20}
-            placeHolder="enter hazard name"
-          />
-          <Text style={{fontSize: 16}}>Primary hazard type</Text>
-          <InputBox
-            height={40}
-            marginHorizontal={20}
-            placeHolder="enter hazard name"
-          />
-          <Text style={{fontSize: 16}}>Nature of hazard</Text>
-          <View
-            style={{
-              marginTop: 12,
-              borderColor: '#D0D0DA',
-              borderWidth: 1,
-              borderRadius: 8,
-            }}>
-            <DropDownPicker items={['Lodu', 'lahsan']} />
+          <Text style={styles.title}>Hazard Details</Text>
+          <View style={styles.fields}>
+            <Text style={styles.label}>Hazard name</Text>
+            <InputBox
+              height={40}
+              marginHorizontal={20}
+              placeHolder="enter hazard name"
+            />
           </View>
+          <View style={styles.fields}>
+            <Text style={styles.label}>Primary hazard type</Text>
+            <InputBox
+              height={40}
+              marginHorizontal={20}
+              placeHolder="enter hazard name"
+            />
+          </View>
+          <View style={styles.fields}>
+            <Text style={styles.label}>Nature of hazard</Text>
+            <View
+              style={{
+                borderColor: '#D0D0DA',
+                borderWidth: 1,
+                borderRadius: 8,
+              }}>
+              <DropDownPicker items={['op']} />
+            </View>
+          </View>
+          <View style={styles.fields}>
+            <Text style={styles.label}>Hazard description</Text>
+            <TextInput
+              style={{
+                height: 150,
+                textAlignVertical: 'top',
+                fontSize: 18,
+                backgroundColor: 'white',
+              }}
+              multiline
+              // keyboardType="visible-password"
+              maxLength={300}
+              numberOfLines={5}
+              // onChangeText={(text) => this.onChangeText(text)}
+              // value={this.state.text}
+              placeholder="Describe the issue here"
+            />
 
-          <Text style={{fontSize: 16}}>Hazard description</Text>
+            <Text style={styles.label}>Photo/Video</Text>
+            <Text>Upload a photo or video or record from your device</Text>
+            <Button
+          title="Select image"
+          onPress={() =>
+            ImagePicker.launchImageLibrary(
+              {
+                mediaType: 'photo',
+                includeBase64: false,
+                maxHeight: 200,
+                maxWidth: 200,
+              },
+              (response) => {
+                setResponse(response);
+              },
+            )
+          }
+        />
 
-          <TextInput
-            style={{
-              height: 150,
-              textAlignVertical: 'top',
-              fontSize: 18,
-              backgroundColor: 'white',
-            }}
-            multiline
-            // keyboardType="visible-password"
-            maxLength={300}
-            numberOfLines={5}
-            // onChangeText={(text) => this.onChangeText(text)}
-            // value={this.state.text}
-            placeholder="Describe the issue here"
-          />
-          <View style={{marginVertical:20}}>
-          <Text style={{fontSize:23,fontWeight:"400"}}>Facility Details</Text>
           </View>
-          <View style={{marginBottom:10}}>
-          <Text>Facility</Text>
+          
+          <Text style={styles.title}>
+            Facility Details
+          </Text>
+          <View style={styles.fields}>
+            <Text style={styles.label}>Facility</Text>
+            <DropDownPicker items={['North London']} />
           </View>
-          <DropDownPicker items={['North London']} />
-          <View style={{marginVertical:10}}>
-          <Text>Department</Text>
+          <View style={styles.fields}>
+            <Text style={styles.label}>Department</Text>
+            <DropDownPicker items={['Department B']}></DropDownPicker>
           </View>
-          <DropDownPicker items={['Department B']}></DropDownPicker>
-          <View style={{marginVertical:10}}>
-          <Text>Location</Text>
+          <View style={styles.fields}>
+            <Text style={styles.label}>Location</Text>
+            <DropDownPicker items={['Area II']}></DropDownPicker>
           </View>
-          <DropDownPicker items={['Area II']}></DropDownPicker>
-          <View style={{marginVertical:10}}>
-          <Text>Sub-Area (optional)</Text>
+          <View style={styles.fields}>
+            <Text style={styles.label}>Sub-Area (optional)</Text>
+            <InputBox height={40} marginHorizontal={20} placeHolder="N/A" />
           </View>
-          <InputBox
-            height={40}
-            marginHorizontal={20}
-            placeHolder="N/A"
-            
-          />
-          <View style={{marginVertical:10}}>
-          <Text>Machine ID (optional)</Text>
-          </View>   
-          <InputBox
-            height={40}
-            marginHorizontal={20}
-            placeHolder="N/A"
-          />
-          <View style={{marginVertical:20}}>
-          <Text style={{fontSize:23,fontWeight:"400"}}>Additional Details</Text>
+          <View style={styles.fields}>
+            <Text style={styles.label}>Machine ID (optional)</Text>
+            <InputBox height={40} marginHorizontal={20} placeHolder="N/A" />
           </View>
-          <View  style={{marginVertical:10}}>
-          <Text>Risk Probability</Text>
+          <Text style={styles.title}>
+            Additional Details
+            </Text>
+          <View style={styles.fields}>
+            <Text style={styles.label}>Risk Probability</Text>
+            <InputBox height={40} marginHorizontal={20} placeHolder="N/A" />
           </View>
-          <InputBox
-            height={40}
-            marginHorizontal={20}
-            placeHolder="N/A"
-          />
-          <View style={{marginVertical:10}}>
-          <Text>Risk Severity</Text>
+          <View style={styles.fields}>
+            <Text style={styles.label}>Risk Severity</Text>
+            <InputBox height={40} marginHorizontal={20} placeHolder="N/A" />
           </View>
-          <InputBox
-            height={40}
-            marginHorizontal={20}
-            placeHolder="N/A"
-          />
-          <View style={{marginVertical:10}}>
-          <Text>Risk Level</Text>
+          <View style={styles.fields}>
+            <Text style={styles.label}>Risk Level</Text>
+            <InputBox
+              disabled={true}
+              height={40}
+              marginHorizontal={20}
+              placeHolder="N/A"
+            />
+            <DropDownPicker items={['View Risk Level Map']}></DropDownPicker>
           </View>
-          <InputBox
-            disabled={true}
-            height={40}
-            marginHorizontal={20}
-            placeHolder="N/A"
-          />
-          <DropDownPicker items={['View Risk Level Map']}></DropDownPicker>
-          <View style={{marginVertical:10}}>
-          <Text>Owner</Text>
+          <View style={styles.fields}>
+            <Text style={styles.label}>Owner</Text>
+            <DropDownPicker items={['Department Manager']}></DropDownPicker>
           </View>
-          <DropDownPicker items={['Department Manager']}></DropDownPicker>
           <TouchableOpacity>
-            <View style={{marginBottom:40,backgroundColor:'blue',width:'100%',marginLeft:'auto',marginRight:'auto',height:40,borderRadius:5,marginTop:10}}>
-         <Text style={{textAlign:'center',color:'white',fontSize:20,marginTop:6}}>Hello</Text>
-         </View>
-         </TouchableOpacity>
+            <View
+              style={{
+                marginBottom: 40,
+                backgroundColor: 'blue',
+                width: '100%',
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                height: 40,
+                borderRadius: 5,
+                marginTop: 10,
+              }}>
+              <Text
+                style={{
+                  textAlign: 'center',
+                  color: 'white',
+                  fontSize: 20,
+                  marginTop: 6,
+                }}>
+                Raise hazard
+              </Text>
+            </View>
+          </TouchableOpacity>
         </ScrollView>
       </View>
     );
@@ -173,7 +212,7 @@ export default class RaiseHazard extends React.PureComponent<Props, State> {
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     backgroundColor: 'blue',
     height: '100%',
   },
@@ -196,5 +235,17 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: 'white',
     marginVertical: 5,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  fields: {
+    marginVertical: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '700',
+    marginVertical: 20
   },
 });
